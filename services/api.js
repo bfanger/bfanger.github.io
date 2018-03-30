3b3682ae5ca8eb0567f54ce70c6f863bbb7ea8cf
Bob Fanger <bfanger@gmail.com>
2018-03-30 16:17:10 +0200

    Switched from axios to fetch

--- services/api.js
+++ services/api.js
@@ -1,13 +1,15 @@
-import axios from "axios";
-
 let endpoint = "/api/";
-if (process.server) {
-  endpoint = "http://localhost:8080/api/";
-}
 
+let fetch;
+if (process.server) {
+  fetch = require("node-fetch");
+  endpoint = "http://localhost:8080/api/";
+} else {
+  fetch = window.fetch;
+}
 async function get(path) {
-  const response = await axios.get(endpoint + path + ".json");
-  return response.data;
+  const response = await fetch(endpoint + path + ".json");
+  return response.json();
 }
 
 export default { get };
