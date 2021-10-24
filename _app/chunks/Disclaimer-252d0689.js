1a11349cd90adef7fce5c5fc4eb075ea699372d3
Bob Fanger <b.fanger@wearetriple.com>
2021-10-24 21:11:10 +0200

    publish: Added beurtbalkje and dice game
    generated from commit f8d03411204eab4fb48fbaa58db9df594d63b513

---
+++ _app/chunks/Disclaimer-252d0689.js
@@ -0,0 +1,5 @@
+import{S as o,i as d,s as c,e as l,t as p,c as j,a as v,g as m,d as n,b as f,f as g,L as _,Q as i}from"./vendor-6dfb7ac5.js";function h(a){let e,r;return{c(){e=l("footer"),r=p(`Deze projecten zijn gerealiseerd tijdens mijn dienstperiode bij de diverse
+  werkgevers en zijn de concept & design fases reeds door overige partijen
+  uitgevoerd.`),this.h()},l(s){e=j(s,"FOOTER",{class:!0});var t=v(e);r=m(t,`Deze projecten zijn gerealiseerd tijdens mijn dienstperiode bij de diverse
+  werkgevers en zijn de concept & design fases reeds door overige partijen
+  uitgevoerd.`),t.forEach(n),this.h()},h(){f(e,"class","disclaimer svelte-11ike7d")},m(s,t){g(s,e,t),_(e,r)},p:i,i,o:i,d(s){s&&n(e)}}}class z extends o{constructor(e){super();d(this,e,null,h,c,{})}}export{z as D};
