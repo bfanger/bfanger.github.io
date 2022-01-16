f5f83caa6d479cf7a06dbb75cf506b9357056236
Bob Fanger <bfanger@gmail.com>
2022-01-16 01:41:27 +0100

    publish: feat: Added skip intro
    generated from commit 6d0b614f59dfd52334fa6b80cdacb192c0965f91

---
+++ _app/pages/swagger-explained/index.svelte-60e88ede.js
@@ -0,0 +1,1 @@
+import{S as s,i as t,s as n}from"../../chunks/vendor-f7287400.js";const l=()=>({status:301,redirect:"https://swagger-explained.bfanger.nl/"});class o extends s{constructor(e){super();t(this,e,null,null,n,{})}}export{o as default,l as load};
