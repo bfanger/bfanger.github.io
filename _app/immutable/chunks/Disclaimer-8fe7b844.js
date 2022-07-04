250cb06cb05787f0dcb017d9770a5d19d06c538d
Bob Fanger <b.fanger@wearetriple.com>
2022-07-04 20:07:20 +0200

    publish: feat: Added svelte-preprocess-react
    generated from commit cfa940f925a983743c373fa449076cabc526e6d8

---
+++ _app/immutable/chunks/Disclaimer-8fe7b844.js
@@ -0,0 +1,5 @@
+import{S as d,i as o,s as l,e as c,t as p,c as m,a as f,h as v,d as n,b as g,g as j,N as h,E as r}from"./index-0a42796d.js";function x(a){let e,i;return{c(){e=c("footer"),i=p(`Deze projecten zijn gerealiseerd tijdens mijn dienstperiode bij de diverse
+  werkgevers en zijn de concept & design fases reeds door overige partijen
+  uitgevoerd.`),this.h()},l(s){e=m(s,"FOOTER",{class:!0});var t=f(e);i=v(t,`Deze projecten zijn gerealiseerd tijdens mijn dienstperiode bij de diverse
+  werkgevers en zijn de concept & design fases reeds door overige partijen
+  uitgevoerd.`),t.forEach(n),this.h()},h(){g(e,"class","disclaimer svelte-11ike7d")},m(s,t){j(s,e,t),h(e,i)},p:r,i:r,o:r,d(s){s&&n(e)}}}class u extends d{constructor(e){super(),o(this,e,null,x,l,{})}}export{u as D};
