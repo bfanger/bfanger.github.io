4c16f6c4a5777539f76a05a0c1972012c3e836d4
Bob Fanger <bfanger@gmail.com>
2022-10-08 15:35:01 +0200

    publish: fix: Optimized perf
    generated from commit 4f3fe9458c0987121d572279d37bcafd8c49a857

--- _app/immutable/chunks/2-55e51d4c.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/scrollytelling/_layout@.svelte-a64574dc.js";const e=!0;export{t as component,e as server};
--- _app/immutable/chunks/7-469d79ab.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./_page-552d9c62.js";import{default as t}from"../components/pages/scrollytelling/_project_/_page.svelte-32c10ce9.js";export{t as component,r as shared};
