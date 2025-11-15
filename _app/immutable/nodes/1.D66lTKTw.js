7b4c8b0e94d37503ebc193bb933cf7916e7806b8
Bob Fanger <bob.fanger@this.nl>
2025-11-15 13:36:09 +0100

    publish: feat: CSS scroll animations
    generated from commit 5662e0be7d957c6e5cb6b8a0b3910ca9a41862a8

---
+++ _app/immutable/nodes/1.D66lTKTw.js
@@ -0,0 +1,1 @@
+import"../chunks/LLdtIxv7.js";import{p as m,a as g,g as e,u as t}from"../chunks/2utecpSq.js";import{p as r}from"../chunks/DGvySZOy.js";import{E as u}from"../chunks/C_A8WkDS.js";function E(s,o){m(o,!0);let a=t(()=>r.status>=400?r.status:"Error"),p=t(()=>r.error?.message||"Oops");u(s,{get title(){return e(a)},get message(){return e(p)}}),g()}export{E as component};
