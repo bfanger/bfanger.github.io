1200b5702ac1054af7991d5981e78f72254712c8
Bob Fanger <bob.fanger@this.nl>
2025-11-15 17:15:31 +0100

    publish: feat: Improved preloading
    generated from commit 9b521e753f936b139effbdcdb6ea46dd23c6129a

--- _app/immutable/chunks/D9QkrnGo.js
+++
@@ -1,1 +0,0 @@
-import{d as p,E as t}from"./2utecpSq.js";import{B as c}from"./LLdtIxv7.js";function E(r,s,...a){var e=new c(r);p(()=>{const n=s()??null;e.ensure(n,n&&(o=>n(o,...a)))},t)}export{E as s};
---
+++ _app/immutable/chunks/ig8rL55c.js
@@ -0,0 +1,1 @@
+import{d as p,E as t}from"./yVz8lnA6.js";import{B as c}from"./ik7tsR6x.js";function E(r,s,...a){var e=new c(r);p(()=>{const n=s()??null;e.ensure(n,n&&(o=>n(o,...a)))},t)}export{E as s};
